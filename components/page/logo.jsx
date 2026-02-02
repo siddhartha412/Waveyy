1df3048bc3721cb83319cecbd3f041e3d8ad294d
Siddhartha412 <siddharthab412@gmail.com>
2026-02-02 22:01:54 +0530

    Update logo text to 'Waveyy'

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,9 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">Music<span className="opacity-50">hub</span></h1>
+                <h1 className="text-2xl font-bold">
+                    <span className="text-blue-500">Wave</span><span className="text-pink-500">yy</span>
+                </h1>
             </div>
         </Link>
     )
