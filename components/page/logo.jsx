e1db69fce35b9f44e2ac7ba795bcae65b74cd1db
Siddhartha412 <siddharthab412@gmail.com>
2026-02-02 22:09:31 +0530

    Update logo.jsx

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,9 +4,9 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-3xl font-extrabold text-gray-700 hover:text-sky-500 transition-all duration-300 ease-in-out">
-                    <span className="text-gray-700">Wave</span>
-                    <span className="text-gray-700">yy</span>
+                <h1 className="text-3xl font-extrabold">
+                    <span className="text-white">Wave</span>
+                    <span className="text-blue-500">yy</span>
                 </h1>
             </div>
         </Link>
